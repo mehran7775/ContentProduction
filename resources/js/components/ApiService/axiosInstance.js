08bacd112edfdfeca78f207a30d1c657b3918e3e
mehran7775 <65393236+mehran7775@users.noreply.github.com>
2021-01-31 23:34:49 +0330

    last changest

--- resources/js/components/ApiService/axiosInstance.js
+++ resources/js/components/ApiService/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  baseURL: '/api'
 });
 // instance.interceptors.response.use(
 //   response=>response,
